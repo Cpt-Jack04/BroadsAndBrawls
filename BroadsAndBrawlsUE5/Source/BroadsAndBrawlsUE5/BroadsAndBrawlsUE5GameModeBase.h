922be80add9189665d698630ced99034833bd33a
Alex Cline <clinealex10@gmail.com>
2023-09-09 18:58:23 -0500

    Project Creation

---
+++ BroadsAndBrawlsUE5/Source/BroadsAndBrawlsUE5/BroadsAndBrawlsUE5GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BroadsAndBrawlsUE5GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BROADSANDBRAWLSUE5_API ABroadsAndBrawlsUE5GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
